cda8891085286fa4dfa58463b86e3b8cd2203822
github1104 <vipboytk98@gmail.com>
2018-11-16 01:29:25 +0700

    ok

---
+++ testphp.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+		echo "hello";
+		$mang = array('haha','hehe','hihi');
+		echo '<pre>';
+		var_dump($mang);
+		echo '</pre>';
+	?>
+</body>
+</html>
